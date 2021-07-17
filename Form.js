a3dff62e8b38465a2b76bdd3eb7e80ff9ac5a5a2
neetikatari <58066915+neetikatari@users.noreply.github.com>
2021-07-17 11:39:18 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,38 @@
+class Form {
+    constructor(){
+        this.title = createElement ("h1")
+        this.button1 = createButton("PLAY")
+        this.button2 = createButton("HOW TO PLAY")
+        
+    }
+    display(){
+       
+        this.title.html("Hunter Warrior")
+        this.title.position(450,70)
+        this.title.style("font-size",500)
+        this.title.style("color","red")
+       
+        this.button1.position(500,150)
+        this.button2.position(500,200)
+    
+        this.button1.mousePressed(()=>{
+            this.button1.hide()
+            this.button2.hide()
+           
+            this.title.hide()
+            
+            gameState=1
+        })
+        this.button2.mousePressed(()=>{
+            gameState=2
+            this.button2.hide()
+            this.button3.hide()
+            
+            this.title.hide()
+          
+            this.button1.position(650,530)
+        })
+        
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# game
+# hunterwarrior
+kill to enemy
+
+# story
+the warrior kill the enemy beacuse they are thief and they are trying stealing the people money.
+
+
+
+
